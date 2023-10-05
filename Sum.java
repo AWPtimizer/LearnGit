39ef1f4a6e608f39c082ee8709b82981e25ffa2a
Mehul Lodha <ahdol4u@gmail.com>
2023-10-05 08:10:30 +0530

    added modified Sum.java

--- Sum.java
+++ Sum.java
@@ -1,8 +1,10 @@
+package LearningGit;
+
 public class Sum {
   public static void main(String[] args) {
     int a = 5;
     int b = 6;
 
-    System.out.println(a + b);
+    System.out.println("The sum of a and b = " + (a + b));
   }
 }
